46452a503cb7838471c44396acce13cf582e833c
Jaroslav Balaz <jaroslav.balaz@otrs.com>
2016-03-15 07:12:49 +0000

    Added new Calendar class.

---
+++ Kernel/System/Calendar.pm
@@ -0,0 +1,64 @@
+# --
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::System::Calendar;
+
+use strict;
+use warnings;
+
+our @ObjectDependencies = (
+
+);
+
+=head1 NAME
+
+Kernel::System::Calendar - calendar lib
+
+=head1 SYNOPSIS
+
+All calendar functions.
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+create an object. Do not use it directly, instead use:
+
+    use Kernel::System::ObjectManager;
+    local $Kernel::OM = Kernel::System::ObjectManager->new();
+    my $CalendarObject = $Kernel::OM->Get('Kernel::System::Calendar');
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+1;
+
+=back
+
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
