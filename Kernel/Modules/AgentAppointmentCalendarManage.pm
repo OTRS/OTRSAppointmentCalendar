3dc5394b85408638206dafd23d108ae42f93601c
Jaroslav Balaz <jaroslav.balaz@otrs.com>
2016-03-17 09:18:31 +0000

    New module - AgentAppointmentCalendarManage

---
+++ Kernel/Modules/AgentAppointmentCalendarManage.pm
@@ -0,0 +1,61 @@
+# --
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
+
+package Kernel::Modules::AgentAppointmentCalendarManage;
+
+use strict;
+use warnings;
+
+our $ObjectManagerDisabled = 1;
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    # get needed objects
+    my $ConfigObject   = $Kernel::OM->Get('Kernel::Config');
+    my $LayoutObject   = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $CalendarObject = $Kernel::OM->Get('Kernel::System::Calendar');
+    my $JSONObject     = $Kernel::OM->Get('Kernel::System::JSON');
+    my $ParamObject    = $Kernel::OM->Get('Kernel::System::Web::Request');
+
+    if ( $Self->{Subaction} eq 'CalendarAdd' ) {
+
+    }
+
+    # get all user's calendars
+    my @Calendars = $CalendarObject->CalendarList(
+        UserID => $Self->{UserID},
+    );
+
+    # get text direction from language object
+    my $TextDirection = $LayoutObject->{LanguageObject}->{TextDirection} || '';
+
+    # output page
+    my $Output = $LayoutObject->Header();
+    $Output .= $LayoutObject->NavigationBar();
+    $Output .= $LayoutObject->Output(
+        TemplateFile => 'AgentAppointmentCalendarManage',
+        Data         => {
+
+        },
+    );
+    $Output .= $LayoutObject->Footer();
+    return $Output;
+}
+
+1;
