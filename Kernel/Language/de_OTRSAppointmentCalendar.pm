9fc7dda47b0d719fbc4608a1758f6725b0e3d3f4
Dusan Vuckovic <dusan.vuckovic@otrs.com>
2016-05-16 13:05:39 +0200

    Update translation.

--- Kernel/Language/de_OTRSAppointmentCalendar.pm
+++ Kernel/Language/de_OTRSAppointmentCalendar.pm
@@ -17,47 +17,67 @@ sub Data {
 
     # Template: AgentAppointmentCalendarManage
     $Self->{Translation}->{'Calendar Management'} = 'Kalenderverwaltung';
-    $Self->{Translation}->{'Calendars'} = 'Kalender';
-    $Self->{Translation}->{'Filter for calendars'} = 'Filter für Kalender';
+    $Self->{Translation}->{'Calendar Overview'} = 'Kalenderübersicht';
     $Self->{Translation}->{'Add new Calendar'} = 'Neuen Kalender anlegen';
+    $Self->{Translation}->{'Calendar Import'} = '';
+    $Self->{Translation}->{'Here you can upload a file to import calendar to your system. The file needs to be in .ics format.'} =
+        '';
+    $Self->{Translation}->{'Upload calendar'} = '';
+    $Self->{Translation}->{'Import calendar'} = '';
+    $Self->{Translation}->{'Filter for calendars'} = 'Filter für Kalender';
+    $Self->{Translation}->{'Depending on the group field, the system will allow users the access to the calendar according to their permission level.'} =
+        '';
+    $Self->{Translation}->{'Read only: users can see and export all appointments in the calendar.'} =
+        '';
+    $Self->{Translation}->{'Move into: users can modify appointments in the calendar, but without changing the calendar selection.'} =
+        '';
+    $Self->{Translation}->{'Create: users can create and delete appointments in the calendar.'} =
+        '';
+    $Self->{Translation}->{'Read/write: users can manage the calendar itself.'} = '';
+    $Self->{Translation}->{'Calendar imported successfully.'} = '';
     $Self->{Translation}->{'Calendar name'} = 'Kalendername';
-    $Self->{Translation}->{'Permission group'} = 'Berechtigungsgruppe';
-    $Self->{Translation}->{'Edit Calendar'} = 'Kalender bearbeiten';
     $Self->{Translation}->{'Calendar with same name already exists.'} = 'Ein Kalender mit gleichem Namen existiert bereits.';
+    $Self->{Translation}->{'Permission group'} = 'Berechtigungsgruppe';
 
     # Template: AgentAppointmentCalendarOverview
+    $Self->{Translation}->{'Manage Calendars'} = 'Kalender verwalten';
+    $Self->{Translation}->{'Calendars'} = 'Kalender';
     $Self->{Translation}->{'This is an overview page for the Appointment Calendar.'} = 'Dies ist eine Übersicht für die Terminkalender.';
-    $Self->{Translation}->{'No calendars found. Please add a calendar first by using Manage Calendars page.'} = 'Keine Kalender gefunden. Bitte legen Sie zuerst einen Kalender über die Kalenderverwaltung an.';
+    $Self->{Translation}->{'No calendars found. Please add a calendar first by using Manage Calendars page.'} =
+        'Keine Kalender gefunden. Bitte legen Sie zuerst einen Kalender über die Kalenderverwaltung an.';
     $Self->{Translation}->{'Week'} = 'Woche';
-    $Self->{Translation}->{'Jump'} = 'Springen';
     $Self->{Translation}->{'Timeline'} = 'Zeitleiste';
+    $Self->{Translation}->{'Jump'} = 'Springen';
+    $Self->{Translation}->{'Appointment'} = 'Termin';
     $Self->{Translation}->{'This is a repeating appointment'} = 'Dieser Termin wiederholt sich';
-    $Self->{Translation}->{'Would you like to edit just this occurrence or all occurrences?'} = 'Möchten Sie nur diesen Termin oder alle Vorkommnisse bearbeiten?';
+    $Self->{Translation}->{'Would you like to edit just this occurrence or all occurrences?'} =
+        'Möchten Sie nur diesen Termin oder alle Vorkommnisse bearbeiten?';
     $Self->{Translation}->{'All occurrences'} = 'Alle Vorkommnisse';
     $Self->{Translation}->{'Just this occurrence'} = 'Nur diesen Termin';
     $Self->{Translation}->{'Dismiss'} = 'Verwerfen';
-
-    # Template: AgentAppointmentCalendarOverviewSeen
-    $Self->{Translation}->{'Ongoing appointments'} = 'Laufende Termine';
-    $Self->{Translation}->{'Following appointments have been started'} = 'Die folgenden Termine haben begonnen';
-    $Self->{Translation}->{'Start time'} = 'Startzeitpunkt';
-    $Self->{Translation}->{'End time'} = 'Endzeitpunkt';
-
-    # Template: AgentAppointmentEdit
     $Self->{Translation}->{'Basic information'} = 'Grundlegende Informationen';
     $Self->{Translation}->{'Date/Time'} = 'Datum/Zeit';
     $Self->{Translation}->{'End date'} = 'Endzeitpunkt';
-    $Self->{Translation}->{'Please set this to a value after start date.'} = 'Bitte setzen Sie hier einen Wert nach dem Startzeitpunkt.';
-    $Self->{Translation}->{'All-day'} = 'Ganztägig';
     $Self->{Translation}->{'Repeat'} = 'Wiederholung';
-    $Self->{Translation}->{'Every Day'} = 'Jeden Tag';
-    $Self->{Translation}->{'Every Week'} = 'Jede Woche';
-    $Self->{Translation}->{'Every Month'} = 'Jeden Monat';
-    $Self->{Translation}->{'Every Year'} = 'Jedes Jahr';
+
+    # Template: AgentAppointmentCalendarOverviewSeen
+    $Self->{Translation}->{'Following appointments have been started'} = 'Die folgenden Termine haben begonnen';
+    $Self->{Translation}->{'Start time'} = 'Startzeitpunkt';
+    $Self->{Translation}->{'End time'} = 'Endzeitpunkt';
+    $Self->{Translation}->{'Resource'} = '';
+
+    # Template: AgentAppointmentEdit
+    $Self->{Translation}->{'Team'} = '';
 
     # Template: AgentAppointmentResourceOverview
+    $Self->{Translation}->{'Resource Overview'} = 'Resourcenübersicht';
+    $Self->{Translation}->{'Manage Teams'} = 'Teams verwalten';
+    $Self->{Translation}->{'Manage Team Agents'} = 'Team-Agenten verwalten';
     $Self->{Translation}->{'This is a resource overview page.'} = 'Dies ist eine Resourcenübersicht.';
-    $Self->{Translation}->{'No teams found. Please add a team first by using Manage Teams page.'} = 'Keine Teams gefunden. Bitte legen Sie zuerst ein Team über die Teamverwaltung an.';
+    $Self->{Translation}->{'No teams found. Please add a team first by using Manage Teams page.'} =
+        'Keine Teams gefunden. Bitte legen Sie zuerst ein Team über die Teamverwaltung an.';
+    $Self->{Translation}->{'No team agents found. Please assign agents to a team first by using Manage Team Agents page.'} =
+        '';
     $Self->{Translation}->{'Timeline Month'} = 'Zeitleiste Monat';
     $Self->{Translation}->{'Timeline Week'} = 'Zeitleiste Woche';
     $Self->{Translation}->{'Timeline Day'} = 'Zeitleiste Tag';
@@ -65,8 +85,9 @@ sub Data {
 
     # Template: AgentAppointmentTeam
     $Self->{Translation}->{'Add Team'} = 'Team hinzufügen';
-    $Self->{Translation}->{'Team added!'} = 'Team hinzugefügt!';
+    $Self->{Translation}->{'Filter for teams'} = '';
     $Self->{Translation}->{'Edit Team'} = 'Team bearbeiten';
+    $Self->{Translation}->{'Team with same name already exists.'} = '';
 
     # Template: AgentAppointmentTeamUser
     $Self->{Translation}->{'Manage Team-Agent Relations'} = 'Team-Agenten-Zuordnungen verwalten';
@@ -74,55 +95,62 @@ sub Data {
 
     # Perl Module: Kernel/Modules/AgentAppointmentCalendarManage.pm
     $Self->{Translation}->{'System was unable to create Calendar!'} = 'Das System konnten den Kalender nicht erstellen!';
-    $Self->{Translation}->{'Please contact the admin.'} = 'Bitte kontaktieren Sie den Administrator.';
     $Self->{Translation}->{'No CalendarID!'} = 'Keine CalenderID!';
     $Self->{Translation}->{'You have no access to this calendar!'} = 'Sie haben keine Zugriffsberechtigung auf diesen Kalender!';
+    $Self->{Translation}->{'Edit Calendar'} = 'Kalender bearbeiten';
     $Self->{Translation}->{'Error updating the calendar!'} = 'Fehler beim Aktualisieren des Kalenders!';
+    $Self->{Translation}->{'No permissions'} = '';
+    $Self->{Translation}->{'No permissions to create a new calendar!'} = '';
+    $Self->{Translation}->{'System was unable to create a new calendar!'} = '';
+    $Self->{Translation}->{'System was unable to import file!'} = '';
 
     # Perl Module: Kernel/Modules/AgentAppointmentEdit.pm
+    $Self->{Translation}->{'Every Day'} = 'Jeden Tag';
+    $Self->{Translation}->{'Every Week'} = 'Jede Woche';
+    $Self->{Translation}->{'Every Month'} = 'Jeden Monat';
+    $Self->{Translation}->{'Every Year'} = 'Jedes Jahr';
     $Self->{Translation}->{'until ...'} = 'bis ...';
     $Self->{Translation}->{'for ... time(s)'} = 'für ... Wiederholungen';
+    $Self->{Translation}->{'No permission!'} = 'Keine Berechtigung!';
     $Self->{Translation}->{'Links could not be deleted!'} = 'Links konnten nicht gelöscht werden!';
     $Self->{Translation}->{'Link could not be created!'} = 'Links konnten nicht erstellt werden!';
     $Self->{Translation}->{'No permissions!'} = 'Keine Berechtigung!';
-    $Self->{Translation}->{'No permission!'} = 'Keine Berechtigung!';
+
+    # Perl Module: Kernel/Modules/AgentAppointmentList.pm
+    $Self->{Translation}->{'Ongoing appointments'} = 'Laufende Termine';
 
     # Perl Module: Kernel/Modules/AgentAppointmentTeamList.pm
     $Self->{Translation}->{'Unassigned'} = 'Nicht zugeordnet';
 
     # Perl Module: Kernel/Modules/PublicCalendar.pm
-    $Self->{Translation}->{'No CalendarID !'} = 'Keine CalenderID!';
-    $Self->{Translation}->{'No UserID !'} = 'Keine UserID!';
     $Self->{Translation}->{'There was an error exporting the calendar!'} = 'Es ist ein Fehler beim Exportieren des Kalenders aufgetreten!';
 
     # SysConfig
     $Self->{Translation}->{'Appointment Calendar overview page.'} = 'Terminkalender Übersicht';
-    $Self->{Translation}->{'Calendar Overview'} = 'Kalenderübersicht';
-    $Self->{Translation}->{'Resource overview screen.'} = 'Resourcenübersichtsseite';
-    $Self->{Translation}->{'Resource Overview'} = 'Resourcenübersicht';
-    $Self->{Translation}->{'Resource overview page.'} = 'Resourcenübersichtsseite';
     $Self->{Translation}->{'Appointment edit screen.'} = 'Terminbearbeitungsansicht';
-    $Self->{Translation}->{'Edit appointment'} = 'Termin bearbeiten';
-    $Self->{Translation}->{'Appointments list.'} = 'Terminliste.';
     $Self->{Translation}->{'Appointment list'} = 'Terminliste';
-    $Self->{Translation}->{'Plugin search module for autocomplete.'} = 'Module zur Pluginsuche für die Autovervollständigung.';
-    $Self->{Translation}->{'Plugin search'} = 'Pluginsuche';
-    $Self->{Translation}->{'Resources list.'} = 'Resourcenliste';
-    $Self->{Translation}->{'Team list'} = 'Teamliste';
+    $Self->{Translation}->{'Appointment list.'} = 'Terminliste.';
+    $Self->{Translation}->{'CalDav'} = '';
     $Self->{Translation}->{'Calendar manage screen.'} = 'Kalenderverwaltungsansicht';
-    $Self->{Translation}->{'Manage Calendars'} = 'Kalender verwalten';
-    $Self->{Translation}->{'Manage different calendars.'} = 'Verschiedene Kalender verwalten';
-    $Self->{Translation}->{'Team management screen.'} = 'Teamverwaltungsansicht';
-    $Self->{Translation}->{'Manage Teams'} = 'Teams verwalten';
-    $Self->{Translation}->{'Team management.'} = 'Teamverwaltung';
-    $Self->{Translation}->{'Team agents management screen.'} = 'Team-Agenten Verwaltungsansicht';
-    $Self->{Translation}->{'Manage Team Agents'} = 'Team-Agenten verwalten';
-    $Self->{Translation}->{'Manage team agents.'} = 'Team-Agenten verwalten';
-    $Self->{Translation}->{'List of colors in hexadecimal RGB which will be allocated to different user calendars. Make sure the colors are dark enough so white text can be overlayed on them. If the number of calendars exceeds the number of colors, the list will be reused from the start.'} = 'Liste an Farben in Hexadezimal RGB, welche verschiedenen Benutzerkalendern zugewiesen werden. Stellen Sie sicher, dass die Farben dunkel genug sind, um weißen Text darauf darzustellen. Sofern die Anzahl der Kalender die Anzahl der verfügbaren Farben überschreitet, wird diese Liste erneut von Anfang an genutzt.';
-    $Self->{Translation}->{'Defines which backend should be used for managing calendars.'} = 'Legt fest, welches Backend für die Kalenderverwaltung genutzt wird.';
     $Self->{Translation}->{'Defines the ticket number plugin for calendar appointments.'} = 'Legt das Plugin für die Ticketnummern innerhalb von Terminkalendern fest.';
-    $Self->{Translation}->{'Ticket Number'} = 'Ticketnummer';
+    $Self->{Translation}->{'Defines which backend should be used for managing calendars.'} =
+        'Legt fest, welches Backend für die Kalenderverwaltung genutzt wird.';
+    $Self->{Translation}->{'Edit appointment'} = 'Termin bearbeiten';
     $Self->{Translation}->{'Links appointments and tickets with a "Normal" type link.'} = 'Verknüpft Termine und Tickets mit dem Typ "Normal".';
+    $Self->{Translation}->{'List of colors in hexadecimal RGB which will be allocated to different user calendars. Make sure the colors are dark enough so white text can be overlayed on them. If the number of calendars exceeds the number of colors, the list will be reused from the start.'} =
+        'Liste an Farben in Hexadezimal RGB, welche verschiedenen Benutzerkalendern zugewiesen werden. Stellen Sie sicher, dass die Farben dunkel genug sind, um weißen Text darauf darzustellen. Sofern die Anzahl der Kalender die Anzahl der verfügbaren Farben überschreitet, wird diese Liste erneut von Anfang an genutzt.';
+    $Self->{Translation}->{'Manage different calendars.'} = 'Verschiedene Kalender verwalten';
+    $Self->{Translation}->{'Manage team agents.'} = 'Team-Agenten verwalten';
+    $Self->{Translation}->{'Plugin search'} = 'Pluginsuche';
+    $Self->{Translation}->{'Plugin search module for autocomplete.'} = 'Module zur Pluginsuche für die Autovervollständigung.';
+    $Self->{Translation}->{'Resource overview page.'} = 'Resourcenübersichtsseite';
+    $Self->{Translation}->{'Resource overview screen.'} = 'Resourcenübersichtsseite';
+    $Self->{Translation}->{'Resources list.'} = 'Resourcenliste';
+    $Self->{Translation}->{'Team agents management screen.'} = 'Team-Agenten Verwaltungsansicht';
+    $Self->{Translation}->{'Team list'} = 'Teamliste';
+    $Self->{Translation}->{'Team management screen.'} = 'Teamverwaltungsansicht';
+    $Self->{Translation}->{'Team management.'} = 'Teamverwaltung';
+
 }
 
 1;
