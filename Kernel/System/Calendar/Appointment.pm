e80035fb6e75727c7ce8d183b452279f7e82d667
Manuel Hecht <manuel.hecht@otrs.com>
2016-11-30 14:53:51 +0100

    Tidied.

--- Kernel/System/Calendar/Appointment.pm
+++ Kernel/System/Calendar/Appointment.pm
@@ -176,7 +176,7 @@ sub AppointmentCreate {
 
         if (
             (
-                $Param{RecurrenceType}    eq 'CustomWeekly'
+                $Param{RecurrenceType} eq 'CustomWeekly'
                 || $Param{RecurrenceType} eq 'CustomMonthly'
                 || $Param{RecurrenceType} eq 'CustomYearly'
             )
@@ -1169,7 +1169,7 @@ sub AppointmentUpdate {
 
         if (
             (
-                $Param{RecurrenceType}    eq 'CustomWeekly'
+                $Param{RecurrenceType} eq 'CustomWeekly'
                 || $Param{RecurrenceType} eq 'CustomMonthly'
                 || $Param{RecurrenceType} eq 'CustomYearly'
             )
